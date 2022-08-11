428e3060ab750c2793450dbc9c983434e0a5acfc
ecdickins <ed.dickins@bleepbloop.net>
2022-08-11 02:15:57 +0100

    text generated on a setTimout, using real content.

--- src/components/TerminalText.js
+++ src/components/TerminalText.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function TerminalText(props) {
+	const ref = React.useRef(null);
 	return (
 		<div className='terminalText'>
 			<p>{props.text}</p>
--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -1,29 +1,41 @@
 import React from 'react';
-import Terminal from './Terminal';
+
 import TerminalText from './TerminalText';
 import { nanoid } from 'nanoid';
 
 export default function TextContainer(props) {
-	let [count, setCount] = React.useState(0);
 	const [terminalTexts, setTerminalTexts] = React.useState([]);
 
 	React.useEffect(() => {
-		setCount(prevCount => prevCount + 1);
-		setTimeout(addTerminalText, 1000);
+		const timer = setTimeout(addTerminalText, 50);
+		return () => clearTimeout(timer);
 	}, [terminalTexts]);
 
-	function addTerminalText() {
-		console.log(`addTerminalText`);
-		console.log(`count=${count}`);
+	async function getMarkovText() {
+		const res = await fetch('http://localhost:5000/api/v1/markovText');
+		const body = await res.json();
+		if (body.success === true) {
+			return body.texts.items[0].txt;
+		}
+	}
 
-		if (count > 5) return;
-
-		const randomNumber = Math.random();
-
+	async function addTerminalText() {
+		const markovText = await getMarkovText();
 		setTerminalTexts(prevTerminalTexts => [
 			...prevTerminalTexts,
-			<TerminalText text={`${count} ${randomNumber}`} key={nanoid()} />,
+			<TerminalText
+				className='terminalText'
+				text={`${markovText}`}
+				key={nanoid()}
+			/>,
 		]);
+		updateScroll();
+	}
+
+	function updateScroll() {
+		terminalTexts.filter(terminalText => {
+			console.log(terminalText.ref);
+		});
 	}
 
 	return <div className='scroller'>{terminalTexts}</div>;
