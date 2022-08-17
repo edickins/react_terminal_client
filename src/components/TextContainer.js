ddf977d58b0f3b338c7724a46ad089b8f1871a81
ecdickins <ed.dickins@bleepbloop.net>
2022-08-17 22:07:58 +0100

    attempting to introduce results from API call to getAscii - currently wrapping getMarkovText() in a getText() method to hide business logic

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -5,6 +5,7 @@ import typingEffect from 'typing-effect';
 
 export default function TextContainer(props) {
 	const [terminalTexts, setTerminalTexts] = React.useState([]);
+	const [multilineTexts, setMultilineTexts] = React.useState([]);
 
 	// filter all terminalText elements accessed by ref
 	// and return all of those reasonably still on screen
@@ -29,26 +30,46 @@ export default function TextContainer(props) {
 		}
 	};
 
+	// hit the API and get multiline text from the express server
+	const getMultilineText = async () => {
+		const res = await fetch('http://localhost:5000/api/v1/ascii');
+		const body = await res.json();
+		if (body.success === true) {
+			console.log(`multiline texts : ${body.texts}`);
+		}
+	};
+
 	// create a new TerminalText element
 	// passing it text, key and ref props
-	const createTerminalTextEl = React.useCallback(async () => {
-		const onScreenElements = getAllTextElementsInView();
-		const ref = React.createRef();
-		const markovText = await getMarkovText();
-		setTerminalTexts([
-			...onScreenElements,
-			<TerminalText
-				className='terminalText'
-				text={`${markovText}`}
-				key={nanoid()}
-				ref={ref}
-			/>,
-		]);
-	}, [getAllTextElementsInView]);
+	const createTerminalTextEl = React.useCallback(
+		async text => {
+			const onScreenElements = getAllTextElementsInView();
+			const ref = React.createRef();
+
+			setTerminalTexts([
+				...onScreenElements,
+				<TerminalText
+					className='terminalText'
+					text={`${text}`}
+					key={nanoid()}
+					ref={ref}
+				/>,
+			]);
+		},
+		[getAllTextElementsInView]
+	);
 
 	// set a timeout after each text animation has completed - or on initial page load
-	const startTimer = () => {
-		setTimeout(createTerminalTextEl, 2000);
+	const startTimer = async () => {
+		getMultilineText();
+		let text = await getText();
+		setTimeout(() => {
+			createTerminalTextEl(text);
+		}, 2000);
+	};
+
+	const getText = async () => {
+		return await getMarkovText();
 	};
 
 	// called once on App startup
