1e6f944277938628787eb188eeae5f35365de449
ecdickins <ed.dickins@bleepbloop.net>
2022-10-17 14:13:29 +0100

    added an initial load so that the terminal has text when it first loads

--- src/components/TerminalText.js
+++ src/components/TerminalText.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 // this functional component is using forwardRef
 // which exposes a ref to itself accessed through el.ref.current
-const TerminalText = React.forwardRef((props, ref) => (
-	<div className={props.className} ref={ref}>
-		<p data-typing-effect>{props.text}</p>
-	</div>
-));
+const TerminalText = React.forwardRef((props, ref) => {
+	if (props.useTypeEffect === true) {
+		return (
+			<div className={props.className} ref={ref}>
+				<p data-typing-effect>{props.text}</p>
+			</div>
+		);
+	} else {
+		return (
+			<div className={props.className} ref={ref}>
+				<p>{props.text}</p>
+			</div>
+		);
+	}
+});
 
 export default TerminalText;
--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import TerminalText from './TerminalText';
-import TerminalTextPre from './TerminalTextPre';
 import { nanoid } from 'nanoid';
 import typingEffect from 'typing-effect';
 
 export default function TextContainer(props) {
-	const [markovTexts, setMarkovTexts] = React.useState([]);
+	const [startAutomatedText, setStartAutomatedText] = React.useState(false);
 	const [terminalTexts, setTerminalTexts] = React.useState([]);
-	const [multilineTexts, setMultilineTexts] = React.useState([]);
 
+	const init = async () => {
+		const texts = await getMarkovText(50);
+		const textEls = texts.map(text => {
+			const ref = React.createRef();
+			return (
+				<TerminalText
+					className='terminalText'
+					text={`${text.txt}`}
+					key={nanoid()}
+					ref={ref}
+					useTypeEffect={false}
+				/>
+			);
+		});
+		setTerminalTexts(prevTextEls => [...prevTextEls, ...textEls]);
+		setStartAutomatedText(true);
+	};
 	// filter all terminalText elements accessed by ref
 	// and return all of those reasonably still on screen
 	// offsetTop value accommodates resized the browser window
@@ -23,29 +38,23 @@ export default function TextContainer(props) {
 		return onScreenElements;
 	}, [terminalTexts]);
 
-	// hit the API and get some Markov Chain generated text
-	const getMarkovText = async () => {
-		try {
-			const res = await fetch(
-				'https://cdn.bleepbloop.net/content/api/v1/markovText'
-			);
-			const body = await res.json();
-			if (body.success === true) {
-				return body.texts.items[0].txt;
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	};
+	// set a timeout after each text animation has completed - or on initial page load
+	const startTimer = React.useCallback(async () => {
+		let textObj = await getText();
 
-	// hit the API and get multiline text from the express server
-	const getMultilineText = async () => {
+		setTimeout(() => {
+			createTerminalTextEl({ text: `${textObj.txt}`, useTypeEffect: true });
+		}, 2000);
+	});
+
+	// hit the API and get some Markov Chain generated text
+	const getMarkovText = async (num = 1) => {
+		const MARKOV_URI = `${process.env.REACT_APP_BASE_URI}/api/v1/markovText?num=${num}`;
 		try {
-			const res = await fetch('http://cdn.bleepbloop.net/content/api/v1/ascii');
+			const res = await fetch(MARKOV_URI);
 			const body = await res.json();
 			if (body.success === true) {
-				console.log(`multiline texts : ${body.texts}`);
-				setMultilineTexts(body.texts.items);
+				return body.texts.items;
 			}
 		} catch (err) {
 			console.log(err);
@@ -55,7 +64,8 @@ export default function TextContainer(props) {
 	// create a new TerminalText element
 	// passing it text, key and ref props
 	const createTerminalTextEl = React.useCallback(
-		async text => {
+		async initObj => {
+			const { text, useTypeEffect } = initObj;
 			const onScreenElements = getAllTextElementsInView();
 			const ref = React.createRef();
 
@@ -66,41 +76,31 @@ export default function TextContainer(props) {
 					text={`${text}`}
 					key={nanoid()}
 					ref={ref}
+					useTypeEffect={useTypeEffect}
 				/>,
 			]);
 		},
 		[getAllTextElementsInView]
 	);
 
-	// set a timeout after each text animation has completed - or on initial page load
-	const startTimer = async (num = 1) => {
-		//if (Math.random() > 0.5 && multilineTexts.length === 0) {
-		//getMultilineText();
-		//}
+	const getText = async (num = 1) => {
+		const texts = await getMarkovText(num);
 
-		let text;
-
-		if (multilineTexts.length > 0) {
-			const item = multilineTexts.shift();
-			text = item.txt;
-		} else {
-			text = await getText();
-		}
-
-		setTimeout(() => {
-			createTerminalTextEl(text);
-		}, 2000);
-	};
-
-	const getText = async () => {
-		const text = await getMarkovText();
-		return text;
+		console.log(`getText text=${texts[0].txt}`);
+		return texts[0];
 	};
 
 	// called once on App startup
 	React.useEffect(() => {
-		startTimer();
+		init();
 	}, []);
+
+	React.useEffect(() => {
+		if (startAutomatedText === true) {
+			startTimer();
+			setStartAutomatedText(false);
+		}
+	}, [startAutomatedText, startTimer]);
 
 	// called every time the screen re-renders.
 	// looks for any paragraphs that need to be animated.
@@ -112,7 +112,7 @@ export default function TextContainer(props) {
 				startTimer();
 			});
 		}
-	});
+	}, [startTimer]);
 
 	// render function
 	return <div className='scroller'>{terminalTexts}</div>;
